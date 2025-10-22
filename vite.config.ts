12db374f207e2ff7c751125e7e86ee670006194b
Yothabo <your_yothabo_email@example.com>
2025-10-22 13:37:40 +0200

    fix: remove external MUI dependency to fix production build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  server: {
+    hmr: {
+      overlay: true,
+    },
+    port: 5173,
+    host: true,
+  },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
   preview: {
     port: 4173,
     host: true
   }
-})
+});
